113aa6fe3c0d33747cba2145861a999125183d91
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2022-10-20 20:49:43 +0200

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,8 @@ import Product from './Product';
 
 export default function Main(props) {
     const {products, onAdd} = props;
-    return <main>
+    return (
+        <main>
         <section className='main block'>
             <h2>Items in stock</h2>
             <div className='products'>
@@ -13,5 +14,6 @@ export default function Main(props) {
             </div>
         </section>
 
-    </main>;
+    </main>
+    );
 }
