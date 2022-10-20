904467db6a31e184c5aab28aad0a1cd1793177fd
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2022-10-20 20:50:26 +0200

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,17 +9,19 @@ export default function Product(props) {
 
     return (
         <div className="singleProduct">
-            <div className={'imageContainer'}>
-                <img className="small" src={product.image} alt={product.title}></img>
-            </div>
-            <div className={" productDetails"}>
-                <h3>{artist}</h3>
-                <p className={'title'}>{title}</p>
-                <p>{product.price} PLN</p>
-                <h5><a className="yt-link" target="_blank" href={product.audio}>YouTube link</a></h5>
-                <div>
+            <div className="imageContainer">
+                
+                    <img className="images" src={product.image} alt={product.title}></img>
+                                </div>
+                                <div className="productDetails">
+                    <h3>{artist}</h3>
+                    <p>{title}</p>
+                    <p>{product.price} PLN</p>
+                    <h5><a className="yt-link" target="_blank" href={product.audio}>YouTube link</a></h5>
+                   
+               
                     <button onClick={() => onAdd(product)}>Add To Cart</button>
-                </div>
+               
 
             </div>
         </div>
