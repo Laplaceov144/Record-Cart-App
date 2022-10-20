071c04f2545cd900693faddb2c50fd28140fcf24
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2022-10-20 20:46:56 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,24 +2,24 @@ const data = {
     products: [
         {
             id: '1',
-            title: 'Beloved - Acid Love',
+            title: 'Primal Scream - Screamadelica',
             price: 100,
-            image: 'https://i.discogs.com/gnkNSHXxn0kqL5Bk-DyGeQ722Uwq7OT6Cgh7LqBakIk/rs:fit/g:sm/q:90/h:381/w:423/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc1NzY0/Mi0xMTU1NzU4ODU5/LmpwZWc.jpeg',
-            audio: 'https://youtu.be/SC29GrHQSIs'
+            image: 'https://i.discogs.com/wBjslZFK5d9SD3cvysK8A-c7mIzGqZYmSgLX7krgJrk/rs:fit/g:sm/q:90/h:456/w:457/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTQ2NDM0/MDMtMTM3MDg2Nzcx/Ni03MjY2LmpwZWc.jpeg',
+            audio: 'https://www.youtube.com/watch?v=kBiMoRM5lfo&list=PLo-CVFFA0DtXbFVErQH0kKHYEvi07sfLK'
         },
         {
             id: '2',
-            title: 'Redshape - Future Shock',
+            title: 'Pixies - Doolittle',
             price: 45,
-            image: 'https://i.discogs.com/0AFn_NFRSk8Flyeu8-esb1KEQoi3AhrvChD4nonQ_Q4/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI1NjE0/NjItMTI5MDUyNjUx/Ni5qcGVn.jpeg',
-            audio: 'https://youtu.be/GmfR7yj5Jz0'
+            image: 'https://i.discogs.com/HG_-j37d1XDln3KMI8iKlehH12kScU-xI93lfMBQ0L0/rs:fit/g:sm/q:90/h:550/w:550/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI4MDU5/MDYtMTM5NjEzNjAx/NS0yMjM3LmpwZWc.jpeg',
+            audio: 'https://www.youtube.com/watch?v=HvldypUz9w8&list=PLCx_l3FfC446YhZSrnYvxRLr3evK943La'
         },
         {
             id: '3',
             title: 'Sonic Youth - Sister',
             price: 60,
             image: 'https://i.discogs.com/LtlzLmbI6nKTXjEvp6hFhWTdBhRLWrF29dkeik0qk5c/rs:fit/g:sm/q:90/h:600/w:578/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEwNTYw/OTMtMTQ5MzY4MTA2/MC0yNTQ0LmpwZWc.jpeg',
-            audio: 'https://youtu.be/sf2qYa8c-cA'
+            audio: 'https://www.youtube.com/watch?v=fWzFkVy3s14&list=PLYwfYGziz-_1lkI1wrxuTlLaalTRPefoQ'
         },
     ]
 };
